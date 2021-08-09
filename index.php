1f4d01978e9b27a3075420d9890432d349188573
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-08-09 17:06:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <html lang="en">
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Wizarding World Movies</title>
+		<title>Wizarding World</title>
 		<!--bootstrap-css-->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 		<!--My css style-->
@@ -35,6 +35,7 @@
                     $conn->close();
                 ?>
             </div>
+            <li><a href="games.php" align=”Right”>Games</a></li>
         </div>
 		<!--bootstrap js&jquery-->
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
