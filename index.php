fd68ec129cc637111c2d3bcde4519c26977998d5
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-08-10 13:56:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,6 @@
                     $conn->close();
                 ?>
             </div>
-            <li><a href="games.php" align=”Right”>Games</a></li>
         </div>
 		<!--bootstrap js&jquery-->
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
